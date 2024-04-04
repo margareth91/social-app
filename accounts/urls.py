1804db96b3613b4e2bb9e0114d59a0b9421d082d
margareth91 <margerytka@wp.eu>
2024-04-04 17:01:37 +0200

    added new logout view url in accounts urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     # path("login/", views.user_login, name="login"),
     # Obecnie używany widok logowania.
     path("login/", auth_views.LoginView.as_view(), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("", views.dashboard, name="dashboard"),
+    path("logout/", views.logout_view, name="logout"),
 ]
