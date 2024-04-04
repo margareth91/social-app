42536b3273a29a9163c8e827d0f7590ecb9e3b14
margareth91 <margerytka@wp.eu>
2024-04-04 15:41:25 +0200

    added missing brackets in login & logout urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     # Poprzednio używany widoki logowania.
     # path("login/", views.user_login, name="login"),
     # Obecnie używany widok logowania.
-    path("login/", auth_views.LoginView.as_view, name="login"),
-    path("logout/", auth_views.LogoutView.as_view, name="logout"),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
