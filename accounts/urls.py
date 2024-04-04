216fd3e7e2b268cd9c15746066ef0c90a0e0ec39
margareth91 <margerytka@wp.eu>
2024-04-04 15:33:10 +0200

    changed used login & logout views in urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,12 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from . import views
+# from . import views
 
 urlpatterns = [
-    # Widoki logowania.
-    path("login/", views.user_login, name="login"),
+    # Poprzednio używany widoki logowania.
+    # path("login/", views.user_login, name="login"),
+    # Obecnie używany widok logowania.
+    path("login/", auth_views.LoginView.as_view, name="login"),
+    path("logout/", auth_views.LogoutView.as_view, name="logout"),
 ]
