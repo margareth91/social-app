da07938cccbf5c38907f5b0b91a5ed431f8a74b9
margareth91 <margerytka@wp.eu>
2024-04-05 13:59:40 +0200

    added register view

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 
 def user_login(request):
@@ -34,3 +34,21 @@ def dashboard(request):
 def logout_view(request):
     logout(request)
     return render(request, "registration/logged_out.html", {})
+
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Utworzenie nowego obiektu użytkownika, bez zapisania w bazie danych.
+            new_user = user_form.save(commit=False)
+            # Ustawienie wybranego hasła.
+            new_user.set_password(user_form.cleaned_data["password"])
+            # Zapisanie obiektu user.
+            new_user.save()
+            return render(
+                request, "accounts/register_done.html", {"new_user": new_user}
+            )
+        else:
+            user_form = UserRegistrationForm()
+            return render(request, "accounts/register.html", {"user_form": user_form})
