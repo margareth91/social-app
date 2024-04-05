7c332c3d50b870603d36d154a959d7f4b2f49164
margareth91 <margerytka@wp.eu>
2024-04-05 11:43:07 +0200

    added urls for password change in accounts urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,4 +10,15 @@ urlpatterns = [
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("", views.dashboard, name="dashboard"),
     path("logout/", views.logout_view, name="logout"),
+    # Adresy URL przeznaczone do obsługi zmiany hasłą.
+    path(
+        "password_change/",
+        auth_views.PasswordChangeView.as_view(),
+        name="password_change",
+    ),
+    path(
+        "password_change/done/",
+        auth_views.PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
 ]
