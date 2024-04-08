0b0a641e93ff93ded52b2352a3660d4fcc31aee0
margareth91 <margerytka@wp.eu>
2024-04-08 15:09:52 +0200

    added edit view

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
 
 
@@ -55,3 +55,23 @@ def register(request):
     else:
         user_form = UserRegistrationForm()
     return render(request, "accounts/register.html", {"user_form": user_form})
+
+
+@login_required
+def edit(request):
+    if request.method == "POST":
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(
+            instance=request.user.profile, data=request.POST, files=request.FILES
+        )
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(
+        request,
+        "accounts/edit.html",
+        {"user_form": user_form, "profile_form": profile_form},
+    )
