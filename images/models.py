a99f1fa3aa8b874b462c8144fb2b08aec4a48d67
margareth91 <margerytka@wp.eu>
2024-04-29 13:52:26 +0200

    corrected typo in image model

--- images/models.py
+++ images/models.py
@@ -5,7 +5,7 @@ from django.utils.text import slugify
 
 class Image(models.Model):
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, related_name="images_creted", on_delete=models.CASCADE
+        settings.AUTH_USER_MODEL, related_name="images_created", on_delete=models.CASCADE
     )
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, blank=True)
