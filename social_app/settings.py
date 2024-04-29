bf781c962acd89ff4eb7ebbde59eecc2c9db8d96
margareth91 <margerytka@wp.eu>
2024-04-29 13:45:59 +0200

    image app start

--- social_app/settings.py
+++ social_app/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
+    'images.apps.ImagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
