b636fe0a5dcb22fdfe6b8c000fef0617e84657d3
margareth91 <margerytka@wp.eu>
2024-05-07 12:53:57 +0200

    Revert "organized correctly models in accounts models"
    This reverts commit 04dd68186896da6bfe04f7f5b5a5f423cfd7500c.

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,18 @@ from django.conf import settings
 from django.db import models
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to="users/%Y/%m/%d", blank=True)
+    following = models.ManyToManyField(
+        "self", through=Contact, related_name="followers", symmetrical=False
+    )
+
+    def __str__(self):
+        return f"Profil użytkownika {self.user.username}"
+
+
 class Contact(models.Model):
     user_from = models.ForeignKey(
         "auth.User", related_name="rel_from_set", on_delete=models.CASCADE
@@ -16,15 +28,3 @@ class Contact(models.Model):
 
     def __str__(self):
         return f"{self.user_from} obserwuje {self.user_to}"
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to="users/%Y/%m/%d", blank=True)
-    following = models.ManyToManyField(
-        "self", through=Contact, related_name="followers", symmetrical=False
-    )
-
-    def __str__(self):
-        return f"Profil użytkownika {self.user.username}"
