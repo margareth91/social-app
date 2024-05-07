16fd624f25a320b284f8eb400224a10c99bb46e5
margareth91 <margerytka@wp.eu>
2024-05-07 16:19:36 +0200

    added target field to model action

--- actions/models.py
+++ actions/models.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 
@@ -7,6 +9,15 @@ class Action(models.Model):
         User, related_name="actions", db_index=True, on_delete=models.CASCADE
     )
     verb = models.CharField(max_length=255)
+    target_ct = models.ForeignKey(
+        ContentType,
+        blank=True,
+        null=True,
+        related_name="target_obj",
+        on_delete=models.CASCADE,
+    )
+    target_id = models.PositiveIntegerField(null=True, blank=True, db_index=True)
+    target = GenericForeignKey("target_ct", "target_id")
     created = models.DateTimeField(auto_now_add=True, db_index=True)
 
     class Meta:
