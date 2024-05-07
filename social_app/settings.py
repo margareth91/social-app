e08bedd2ca81744a518ee135cc650e53335c0307
margareth91 <margerytka@wp.eu>
2024-05-07 16:02:58 +0200

    actions app start

--- social_app/settings.py
+++ social_app/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'images.apps.ImagesConfig',
+    'actions.apps.ActionsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
