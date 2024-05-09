4ad15b39c54305cf6442e4c0f2843887796baac9
margareth91 <margerytka@wp.eu>
2024-05-09 13:35:20 +0200

    added class meta to model image in images app

--- images/models.py
+++ images/models.py
@@ -30,3 +30,6 @@ class Image(models.Model):
 
     def get_absolute_url(self):
         return reverse("images:detail", args=[self.id, self.slug])
+
+    class Meta:
+        ordering = ('-created',)
